98f19173818cb1312841787897db850b8389615d
Theo Albers <theo.albers@outlook.com>
2014-11-14 23:43:18 +0100

    Skeleton

---
+++ Web.Resources.MsTest/CustomersControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayMe.DomainModel.Customers;
+using Rhino.Mocks;
+
+namespace PayMe.Web.Resources
+{
+    [TestClass]
+    public class CustomersControllerTest
+    {
+        [TestMethod]
+        public void GetAll_ShouldReturnOne()
+        {
+            // Setup
+            var mock = MockRepository.GenerateMock<ICustomerRepository>();
+            mock.Expect(x => x.GetAll())
+                .Return(new Customer[] 
+                { 
+                    new Customer() 
+                    { 
+                        Id = 1, 
+                        Name = "Pipo" 
+                    } 
+                })
+                .Repeat.Once();
+            var subject = new CustomersController(mock);
+
+            // Run test
+            var result = subject.GetAll();
+
+            // Post conditions
+            mock.VerifyAllExpectations(); // doesn't work
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
---
+++ Web.Resources/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using PayMe.DomainModel.Customers;
+
+namespace PayMe.Web.Resources
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ICustomerRepository _repository;
+
+        public CustomersController(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
