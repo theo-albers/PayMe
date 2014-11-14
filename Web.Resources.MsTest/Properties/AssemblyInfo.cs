dd9528143ed3ec8f86c8550a883fd5e751bf834c
Theo Albers <theo.albers@outlook.com>
2014-11-14 23:35:51 +0100

    Skeleton

---
+++ Web.Resources.MsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PayMe.Web.Resources.MsTest")]
+[assembly: AssemblyDescription("PayMe.Web.Resources.MsTest")]
+[assembly: Guid("5fa35506-70c7-4ed0-a182-e5c3c5b4a580")]
+
---
+++ Web.Resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PayMe.Web.Resources")]
+[assembly: AssemblyDescription("PayMe.Web.Resources")]
+[assembly: Guid("d17043b6-db3d-42e9-b39a-3c25049d8d9f")]
+
